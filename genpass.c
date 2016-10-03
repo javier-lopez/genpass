868a8b61a173e0ecb2347de0115835c9157487bd
Javier López <m@javier.io>
2016-10-03 11:16:25 -0500

    add cache_file as a valid option in configuration file

--- genpass.c
+++ genpass.c
@@ -56,6 +56,7 @@ typedef struct config_params {
     char *password;
     char *site;
     char *encoding;
+    char *cache_file;
 } configuration;
 
 void version(void) {
@@ -82,7 +83,7 @@ void usage(int status) {
       \n  -e, --encoding ENCODING   password encoding output, \""DEFAULT_ENCODING"\" by default\
       \n                              ENCODING: dec|hex|base64|z85|skey\
       \n  -1, --single              use single function derivation\
-      \n      --config              configuration file\
+      \n      --config FILE         configuration file\
       \n\
       \n  -v, --verbose             verbose mode\
       \n  -V, --version             show version and exit\
@@ -187,6 +188,8 @@ static int config_handler (void* user, const char* section, const char* name,
         pconfig->site = strdup(value);
     } else if (MATCH("user", "password")) {
         pconfig->password = strdup(value);
+    } else if (MATCH("general", "cache_file")) {
+        pconfig->cache_file = strdup(value);
     } else if (MATCH("general", "keylen")) {
         pconfig->keylen = strdup(value);
     } else if (MATCH("general", "cache_cost")) {
@@ -318,9 +321,9 @@ int main(const int argc, const char * const argv[]) {
     char * name                                 = NULL;
     char * password                             = NULL;
     char * site                                 = NULL;
-    char * config                               = NULL;
+    char * config_file                          = NULL;
     char registration_mode                      = 0;
-    const char * filename                       = NULL;
+    const char * cache_file                     = NULL;
     int  keylen                                 = SCRYPT_HASH_LEN;
     int  cache_cost                             = SCRYPT_CACHE_COST;
     int  cost                                   = SCRYPT_COST;
@@ -379,7 +382,7 @@ int main(const int argc, const char * const argv[]) {
 
     //process options
     if (!ap_init(&parser, argc, argv, options, 0))
-        die("Not enough memory.", 0, 0);
+        die("not enough memory.", 0, 0);
     //unrecognized option
     if (ap_error(&parser)) die(ap_error(&parser), 0, 1);
 
@@ -392,11 +395,11 @@ int main(const int argc, const char * const argv[]) {
                 case 'p': if (arg[0]) { password = (char *) arg; } break;
                 case 's': if (arg[0]) { site     = (char *) arg; } break;
                 case 202: if (arg[0]) {
-                    is_config = true;
-                    config = (char *) arg;
+                    is_config   = true;
+                    config_file = (char *) arg;
                 } break;
                 case 'r': registration_mode = 1; break;
-                case 'f': if (arg[0]) { filename = arg; } break;
+                case 'f': if (arg[0]) { cache_file = arg; } break;
                 case 'l': check_option(code, arg, &keylen);
                     break;
                 case 'C': check_option(code, arg, &cache_cost);
@@ -421,13 +424,16 @@ int main(const int argc, const char * const argv[]) {
 
     //check if configuration file is provided
     if (is_config) {
-        if (ini_parse(config, config_handler, &conf) < 0) {
-            die("Can not load config file.", 0, 0);
+        if (ini_parse(config_file, config_handler, &conf) < 0) {
+            snprintf(error_msg, sizeof error_msg,
+                     "couldn't load config file '%s'", config_file);
+            die(error_msg, 0, 1);
         }
 
-        if (conf.name) { name = conf.name;}
-        if (conf.site) { site = conf.site;}
-        if (conf.password) { password = conf.password;}
+        if (conf.name)       {name = conf.name;}
+        if (conf.site)       {site = conf.site;}
+        if (conf.password)   {password = conf.password;}
+        if (conf.cache_file) {cache_file = conf.cache_file;}
 
         if (conf.keylen)
             check_option('l', (const char * const) conf.keylen, &keylen);
@@ -462,10 +468,10 @@ int main(const int argc, const char * const argv[]) {
         }
     }
 
-    if (filename == NULL) {
+    if (cache_file == NULL) {
         if ((homedir = getenv("HOME")) != NULL) {
             snprintf(fpath, sizeof fpath, "%s/%s", homedir, ".genpass-cache");
-            filename = fpath;
+            cache_file = fpath;
         } else {
             fprintf(stderr, "Warning: unable to determinate HOME directory, ");
             fprintf(stderr, "falling to --dry-mode ...\n");
@@ -505,8 +511,8 @@ int main(const int argc, const char * const argv[]) {
     scrypt_n       = _pow(2,cost);
 
     if (!single_function_derivation && !dry_run) {
-        fp = fopen(filename, "rb");
-        snprintf(verbose_msg, sizeof(verbose_msg), "Trying to open %s", filename);
+        fp = fopen(cache_file, "rb");
+        snprintf(verbose_msg, sizeof(verbose_msg), "Trying to open %s", cache_file);
         verbose(verbose_msg, verbose_lvl);
         if (fp!=NULL) {
             verbose("File open, attempting to read cache key", verbose_lvl);
@@ -522,7 +528,7 @@ int main(const int argc, const char * const argv[]) {
                 readbytes = 0; for (i = 0; i < (cache_cost+scrypt_r+scrypt_p); i++)
                     readbytes = fread(cache_hashbuf,1,keylen,fp);
                 if (readbytes != keylen) {
-                    fprintf(stderr, "Warning: error while reading %s, ", filename);
+                    fprintf(stderr, "Warning: error while reading %s, ", cache_file);
                     fprintf(stderr, "falling to --dry-mode ...\n");
                     dry_run = 1;
                 } else {
@@ -556,13 +562,13 @@ int main(const int argc, const char * const argv[]) {
 
     if (!single_function_derivation && !dry_run && !cache_hash_in_file) {
         snprintf(verbose_msg, sizeof(verbose_msg), \
-            "Attempting to save cache key to %s", filename);
+            "Attempting to save cache key to %s", cache_file);
         verbose(verbose_msg, verbose_lvl);
-        fp = fopen(filename, "wb");
+        fp = fopen(cache_file, "wb");
         if (fp!=NULL) {
             for (i = 0; i < (cache_cost+scrypt_r+scrypt_p); i++) {
                 if (fwrite(&cache_hashbuf, keylen, 1, fp) != 1) {
-                    fprintf(stderr, "Warning: error while writing %s, ", filename);
+                    fprintf(stderr, "Warning: error while writing %s, ", cache_file);
                     fprintf(stderr, "falling to --dry-mode ...\n");
                     dry_run = 1;
                     break;
