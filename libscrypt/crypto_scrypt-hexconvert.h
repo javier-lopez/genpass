fa096d40ad4655c1732ed06690213cb0040fcacf
asmita005 <asmitamittal05@gmail.com>
2016-10-04 08:20:09 +0530

    Delete crypto_scrypt-hexconvert.h

--- libscrypt/crypto_scrypt-hexconvert.h
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-#include <stdint.h>
-
-/**
- * Converts a binary string to a hex representation of that string
- * outbuf must have size of at least buf * 2 + 1.
- */
-int libscrypt_hexconvert(const unsigned char * buf, size_t s, char *outbuf, size_t obs);
-int libscrypt_hex_encode(const unsigned char * buf, size_t s, char *outbuf, size_t obs);
